7346e35abba50054eaec7844f221b930f86993cf
奕安 <cv315122@gmail.com>
2024-03-18 21:25:40 +0800

    修改部分排版樣式

--- src/app/_components/slider.tsx
+++ src/app/_components/slider.tsx
@@ -4,8 +4,6 @@ import Image from "next/image";
 import { Swiper, SwiperSlide } from "swiper/react";
 
 import "swiper/css";
-import "swiper/css/pagination";
-import "swiper/css/navigation";
 
 import { computersDatas } from "@/app/_dataSets/data";
 
@@ -33,11 +31,11 @@ export const SwiperContent: React.FC = () => {
           spaceBetween: 30,
         },
       }}
-      className="relative flex w-[75%] min-h-[680px]"
+      className="relative flex w-[75%] min-h-[680px] rounded-xl"
     >
       {computersDatas.map((data: ComputerData, index: number) => (
         <SwiperSlide
-          className="flex flex-col space-y-2 w-[350px] p-2 bg-white border select-none"
+          className="flex flex-col items-center mt-4 w-[350px] bg-white border rounded-xl select-none shadow-outline"
           key={index}
         >
           <div className="p-5 relative flex flex-col gap-2">
@@ -63,15 +61,15 @@ export const SwiperContent: React.FC = () => {
                 {data.productName}
               </h2>
 
-              <ul className="flex flex-col items-start gap-2 mt-10 text-[#666]">
-                <li className="list-none whitespace-normal text-start text-sm">
+              <div className="flex flex-col items-start gap-2 mt-10 text-[#666]">
+                <span className="list-none text-start text-sm whitespace-pre-line">
                   {data.description}
-                </li>
-              </ul>
+                </span>
+              </div>
             </div>
           </div>
 
-          <div className="flex flex-col justify-between rounded-b-[10px] bg-slate-150 p-5">
+          <div className="flex flex-col bg-[#f2f6fa] p-5">
             <div className="flex gap-1 bg-[#f00] text-[#f7f7f7] text-[12px] rounded-full p-2 w-fit">
               <span>SAVE</span>
               <span>${data.discount}</span>
@@ -94,14 +92,14 @@ export const SwiperContent: React.FC = () => {
             </button>
           </div>
 
-          <div className="flex justify-between items-center bg-slate-150 px-5 py-2">
+          <div className="flex justify-between items-center bg-[#f2f6fa] px-5 py-2">
             <div className="flex flex-col items-start">
               <span className="text-xs font-bold">Free Shipping</span>
               <span className="text-xs">{data.freeShipping}</span>
             </div>
 
             <div className="buy-btn flex justify-center border border-[#f00] hover:bg-[#f00]  rounded-full w-24 h-fit py-1 bg-transparent transition-all duration-[400ms]">
-              <button className="text-[#f00] hover:text-white transition-all duration-[400ms]">
+              <button className="text-[#f00] hover:text-white text-sm transition-all duration-[400ms]">
                 {data.buttonTexts}
               </button>
             </div>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,10 +8,8 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      boxShadow: {
+        outline: "0px 1px 3px rgba(0, 0, 0, 0.2)",
       },
     },
   },
